ebb0db86b5dd8c3980a25dc24a5f0092172337d5
De Ioanni, Giuliano <giuliano.deioanni@gft.com>
2021-10-14 14:40:01 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
